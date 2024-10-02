3b73b1b9c535f250a363e5df2153f3a8002aab30
Vinh Phat <caovinhphat1904@gmail.com>
2024-10-02 08:38:24 +0700

    done ex9

--- stm32Prj_lab2/Core/Src/exercise1.c
+++ stm32Prj_lab2/Core/Src/exercise1.c
@@ -244,49 +244,41 @@ void updateLEDMatrix(int index){
         	clearMatrix();
         	GPIOA->ODR = matrix_buffer[index];
         	GPIOB->ODR = A_buffer[index];
-        	//HAL_GPIO_WritePin(ENM0_GPIO_Port, ENM0_Pin, GPIO_PIN_SET);
             break;
         case 1:
         	clearMatrix();
         	GPIOA->ODR = matrix_buffer[index];
         	GPIOB->ODR = A_buffer[index];
-        	//HAL_GPIO_WritePin(ENM1_GPIO_Port, ENM1_Pin, GPIO_PIN_SET);
             break;
         case 2:
         	clearMatrix();
         	GPIOA->ODR = matrix_buffer[index];
         	GPIOB->ODR = A_buffer[index];
-        	//HAL_GPIO_WritePin(ENM2_GPIO_Port, ENM2_Pin, GPIO_PIN_SET);
             break;
         case 3:
         	clearMatrix();
         	GPIOA->ODR = matrix_buffer[index];
         	GPIOB->ODR = A_buffer[index];
-        	//HAL_GPIO_WritePin(ENM3_GPIO_Port, ENM3_Pin, GPIO_PIN_SET);
             break;
         case 4:
         	clearMatrix();
         	GPIOA->ODR = matrix_buffer[index];
         	GPIOB->ODR = A_buffer[index];
-        	//HAL_GPIO_WritePin(ENM4_GPIO_Port, ENM4_Pin, GPIO_PIN_SET);
             break;
         case 5:
         	clearMatrix();
         	GPIOA->ODR = matrix_buffer[index];
         	GPIOB->ODR = A_buffer[index];
-        	//HAL_GPIO_WritePin(ENM5_GPIO_Port, ENM5_Pin, GPIO_PIN_SET);
             break;
         case 6:
         	clearMatrix();
         	GPIOA->ODR = matrix_buffer[index];
         	GPIOB->ODR = A_buffer[index];
-        	//HAL_GPIO_WritePin(ENM6_GPIO_Port, ENM6_Pin, GPIO_PIN_SET);
             break;
         case 7:
         	clearMatrix();
         	GPIOA->ODR = matrix_buffer[index];
         	GPIOB->ODR = A_buffer[index];
-        	//HAL_GPIO_WritePin(ENM7_GPIO_Port, ENM7_Pin, GPIO_PIN_SET);
             break;
         default:
             break;
